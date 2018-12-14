30f139543655fa3e5acfd1a8a91ffaf95df6e104
gaoljhy <lenqu666@outlook.com>
2018-12-14 11:47:14 +0800

    http

--- 白/学习类/语言类高级/C++/Socket/TCP/serve.cpp
+++ 白/学习类/语言类高级/C++/Socket/TCP/serve.cpp
@@ -1,73 +1,59 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
- 
- 
-int main(int argc, char *argv[])
-{
-	int server_sockfd;//服务器端套接字
-	int client_sockfd;//客户端套接字
-	int len;
-	struct sockaddr_in my_addr;   //服务器网络地址结构体
-	struct sockaddr_in remote_addr; //客户端网络地址结构体
-	int sin_size;
-	char buf[BUFSIZ];  //数据传送的缓冲区
-	memset(&my_addr,0,sizeof(my_addr)); //数据初始化--清零
-	my_addr.sin_family=AF_INET; //设置为IP通信
-	my_addr.sin_addr.s_addr=INADDR_ANY;//服务器IP地址--允许连接到所有本地地址上
-	my_addr.sin_port=htons(8000); //服务器端口号
-	
-	/*创建服务器端套接字--IPv4协议，面向连接通信，TCP协议*/
-	if((server_sockfd=socket(PF_INET,SOCK_STREAM,0))<0)
-	{  
-		perror("socket error");
-		return 1;
-	}
- 
- 
-	/*将套接字绑定到服务器的网络地址上*/
-	if(bind(server_sockfd,(struct sockaddr *)&my_addr,sizeof(struct sockaddr))<0)
-	{
-		perror("bind error");
-		return 1;
-	}
-	
-	/*监听连接请求--监听队列长度为5*/
-	if(listen(server_sockfd,5)<0)
-	{
-		perror("listen error");
-		return 1;
-	};
-	
-	sin_size=sizeof(struct sockaddr_in);
-	
-	/*等待客户端连接请求到达*/
-	if((client_sockfd=accept(server_sockfd,(struct sockaddr *)&remote_addr,&sin_size))<0)
-	{
-		perror("accept error");
-		return 1;
-	}
-	printf("accept client %s/n",inet_ntoa(remote_addr.sin_addr));
-	len=send(client_sockfd,"Welcome to my server/n",21,0);//发送欢迎信息
-	
-	/*接收客户端的数据并将其发送给客户端--recv返回接收到的字节数，send返回发送的字节数*/
-	while((len=recv(client_sockfd,buf,BUFSIZ,0))>0))
-	{
-		buf[len]='/0';
-		printf("%s/n",buf);
-		if(send(client_sockfd,buf,len,0)<0)
-		{
-			perror("write error");
-			return 1;
-		}
-	}
- 
- 
-	/*关闭套接字*/
-	close(client_sockfd);
-	close(server_sockfd);
-    
-	return 0;
-}
+#include  <unistd.h> 
+#include<stdio.h>  
+#include<stdlib.h>  
+#include<string.h>  
+#include<errno.h>  
+#include<sys/types.h>  
+#include<sys/socket.h>  
+#include<netinet/in.h>  
+#define DEFAULT_PORT 8000  
+#define MAXLINE 4096  
+int main(int argc, char** argv)  
+{  
+    int    socket_fd, connect_fd;  
+    struct sockaddr_in     servaddr;  
+    char    buff[4096];  
+    int     n;  
+    //初始化Socket  
+    if( (socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){  
+    printf("create socket error: %s(errno: %d)\n",strerror(errno),errno);  
+    exit(0);  
+    }  
+    //初始化  
+    memset(&servaddr, 0, sizeof(servaddr));  
+    servaddr.sin_family = AF_INET;  
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);//IP地址设置成INADDR_ANY,让系统自动获取本机的IP地址。  
+    servaddr.sin_port = htons(DEFAULT_PORT);//设置的端口为DEFAULT_PORT  
+  
+    //将本地地址绑定到所创建的套接字上  
+    if( bind(socket_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1){  
+    printf("bind socket error: %s(errno: %d)\n",strerror(errno),errno);  
+    exit(0);  
+    }  
+    //开始监听是否有客户端连接  
+    if( listen(socket_fd, 10) == -1){  
+    printf("listen socket error: %s(errno: %d)\n",strerror(errno),errno);  
+    exit(0);  
+    }  
+    printf("======waiting for client's request======\n");  
+    while(1){  
+//阻塞直到有客户端连接，不然多浪费CPU资源。  
+        if( (connect_fd = accept(socket_fd, (struct sockaddr*)NULL, NULL)) == -1){  
+        printf("accept socket error: %s(errno: %d)",strerror(errno),errno);  
+        continue;  
+    }  
+//接受客户端传过来的数据  
+    n = recv(connect_fd, buff, MAXLINE, 0);  
+//向客户端发送回应数据  
+    if(!fork()){ /*紫禁城*/  
+        if(send(connect_fd, "Hello,you are connected!\n", 26,0) == -1)  
+        perror("send error");  
+        close(connect_fd);  
+        exit(0);  
+    }  
+    buff[n] = '\0';  
+    printf("recv msg from client: %s\n", buff);  
+    close(connect_fd);  
+    }  
+    close(socket_fd);  
+} 
